5fc0978a4abdff6bda71eb25a12f1c2a23171427
DANIEL <107651766+Anuebunwa-Daniel@users.noreply.github.com>
2022-09-10 20:40:36 +0100

    Initial commit

---
+++ mailSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const mongodb = 'mongodb+srv://dahumble:anuebunwada1.@mailer.7oecdwf.mongodb.net/my projects'
+mongoose.connect(mongodb)
+
+const mailSchema = new mongoose.Schema({
+    from:String,
+    password:String,
+    email:String,
+    subject:String,
+    mail:String
+})
+
+module.exports = mongoose.model('mail', mailSchema)
